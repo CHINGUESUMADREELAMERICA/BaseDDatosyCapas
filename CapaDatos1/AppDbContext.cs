b00ff415f95e537ec2c2ae4c9d6ba4d3a6f0938e
ExterminadoraChimichanga <equizdedospuntosuve@gmail.com>
2026-06-06 21:01:27 -0700

    zi

--- CapaDatos1/AppDbContext.cs
+++ CapaDatos1/AppDbContext.cs
@@ -19,8 +19,8 @@ namespace CapaDatos1
             options.UseSqlServer(
                 //si quieren construirlo en sus bases de datos solo cambien el nombre del servidor y
                 //en la consola de herramientas nuget ponen Add-Migration Inicial y luego Update-Database
-                //"Server=(localdb)\\MSSQLLocalDB; Database=TerrenosDB; Trusted_Connection = True;TrustServerCertificate=true;"//Diana
-                //"Server=Maquiavelica1\\SQLEXPRESS;Database=TerrenosDB; Trusted_Connection = True;TrustServerCertificate=true;"//Camila
+                //"Server=(localdb)\\MSSQLLocalDB; Database=TerrenosDB; Trusted_Connection = True;TrustServerCertificate=true;"
+                //"nombreServidor\\SQLEXPRESS;Database=TerrenosDB; Trusted_Connection = True;TrustServerCertificate=true;"
             );
         }
     }
